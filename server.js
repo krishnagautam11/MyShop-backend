241b20ac1e877ff6a14811e4b6570fe8bce0fef8
krishnagautam11 <93446439+krishnagautam11@users.noreply.github.com>
2025-05-17 00:00:47 +0530

    Fix CORS setup to allow frontend URL

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://my-shop-frontend-4r3lt2mqg-krishna-gautams-projects-4f6e85c9c.vercel.app',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
