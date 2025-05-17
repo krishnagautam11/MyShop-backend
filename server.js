608e26c2b3c8649d34fe11dfe387e2aec0bb2e90
krishnagautam11 <93446439+krishnagautam11@users.noreply.github.com>
2025-05-17 17:08:11 +0530

    Add root route to server to handle GET / requests

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ app.use('/uploads', express.static(path.join(__dirname, 'public/uploads'), {
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.listen(PORT, ()=>{
     console.log(`server is running`);
 });
